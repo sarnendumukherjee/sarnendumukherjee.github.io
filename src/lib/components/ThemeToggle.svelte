aa733521fa88d84878234462f1036b6a319d4046
Sarnendu Mukherjee <sarnendu.mukherjee@gmail.com>
2025-07-03 20:00:46 +0200

    feat: add lucide-svelte for theme icons and replace SVGs in ThemeToggle component

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { theme, toggleTheme } from '../stores/theme';
+  import { Sun, Moon } from 'lucide-svelte';
 </script>
 
 <button
@@ -8,34 +9,8 @@
   aria-label="Toggle theme"
 >
   {#if $theme === 'light'}
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      stroke-width="2"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M12 3v1m0 16v1m9-9h1M3 12h1m15.325-7.757l-.707.707M5.382 18.325l-.707.707M18.325 5.382l.707-.707M5.382 5.382l.707-.707M12 18a6 6 0 100-12 6 6 0 000 12z"
-      />
-    </svg>
+    <Sun class="h-6 w-6" />
   {:else}
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      stroke-width="2"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-      />
-    </svg>
+    <Moon class="h-6 w-6" />
   {/if}
 </button>
