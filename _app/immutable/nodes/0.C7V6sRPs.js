f4dcd2c7376b90ef2aa5365dcb1631f7830fc6a4
Sarnendu Mukherjee <sarnendu.mukherjee@gmail.com>
2025-07-03 17:38:40 +0200

    Updates

--- _app/immutable/nodes/0.C7V6sRPs.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/CWj6FrbW.js";import{Q as n,R as a,S as p}from"../chunks/LzAq6wT6.js";import{s}from"../chunks/DlyGMHpH.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
