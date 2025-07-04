67ab15779a785267e12a7bc0cbae6ffdf7f7aafe
Sarnendu Mukherjee <sarnendu.mukherjee@gmail.com>
2025-07-04 11:51:28 +0200

    Updates

---
+++ _app/immutable/nodes/0.CxUHvf4m.js
@@ -0,0 +1,77 @@
+import"../chunks/CWj6FrbW.js";import{aE as G,aB as g,a2 as m,a4 as f,a1 as k,aF as _e,a6 as l,a7 as n,aG as pe,a3 as V,aH as ge,T as X,u as Y,aA as me,a5 as u,V as q,am as J,Z as I}from"../chunks/A9ibgEUH.js";import{s as ke}from"../chunks/CEAjniT4.js";import{I as x,s as C,a as xe,c as Ce,b as be}from"../chunks/BgiBJchZ.js";import{l as b,s as y,i as B,a as K,c as O,p as ye,b as $e}from"../chunks/paGwL3ZV.js";import"../chunks/Bsqwy2y4.js";const we=!0,rt=Object.freeze(Object.defineProperty({__proto__:null,prerender:we},Symbol.toStringTag,{value:"Module"})),Me=typeof window<"u"&&window.matchMedia("(prefers-color-scheme: dark)").matches,Le=typeof localStorage<"u"&&localStorage.getItem("theme")||(Me?"dark":"light"),$=G(Le);$.subscribe(t=>{typeof localStorage<"u"&&localStorage.setItem("theme",t),typeof document<"u"&&(t==="dark"?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark"))});function T(){$.update(t=>t==="light"?"dark":"light")}const Q=G(0);function De(t,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const r=[["path",{d:"M4 12h16"}],["path",{d:"M4 18h16"}],["path",{d:"M4 6h16"}]];x(t,y({name:"menu"},()=>s,{get iconNode(){return r},children:(a,v)=>{var o=g(),c=m(o);C(c,e,"default",{}),f(a,o)},$$slots:{default:!0}}))}function Ee(t,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const r=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"}]];x(t,y({name:"moon"},()=>s,{get iconNode(){return r},children:(a,v)=>{var o=g(),c=m(o);C(c,e,"default",{}),f(a,o)},$$slots:{default:!0}}))}function Ne(t,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const r=[["circle",{cx:"12",cy:"12",r:"4"}],["path",{d:"M12 2v2"}],["path",{d:"M12 20v2"}],["path",{d:"m4.93 4.93 1.41 1.41"}],["path",{d:"m17.66 17.66 1.41 1.41"}],["path",{d:"M2 12h2"}],["path",{d:"M20 12h2"}],["path",{d:"m6.34 17.66-1.41 1.41"}],["path",{d:"m19.07 4.93-1.41 1.41"}]];x(t,y({name:"sun"},()=>s,{get iconNode(){return r},children:(a,v)=>{var o=g(),c=m(o);C(c,e,"default",{}),f(a,o)},$$slots:{default:!0}}))}function Se(t,e){const s=b(e,["children","$$slots","$$events","$$legacy"]);/**
+ * @license lucide-svelte v0.525.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */const r=[["path",{d:"M18 6 6 18"}],["path",{d:"m6 6 12 12"}]];x(t,y({name:"x"},()=>s,{get iconNode(){return r},children:(a,v)=>{var o=g(),c=m(o);C(c,e,"default",{}),f(a,o)},$$slots:{default:!0}}))}var ze=k('<button class="p-2 rounded-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-500 transition-colors duration-300" aria-label="Toggle theme"><!></button>');function F(t){const[e,s]=K(),r=()=>O($,"$theme",e);var a=ze(),v=l(a);{var o=d=>{Ne(d,{class:"h-6 w-6"})},c=d=>{Ee(d,{class:"h-6 w-6"})};B(v,d=>{r()==="light"?d(o):d(c,!1)})}n(a),_e("click",a,function(...d){T==null||T.apply(this,d)}),f(t,a),s()}var He=pe('<svg xmlns="http://www.w3.org/2000/svg" width="175" height="150" viewBox="0 0 175 150" version="1.1"><path d="M 3 6.393 C 3 8.083, 5.157 9, 9.131 9 C 13.402 9, 17.695 11.538, 19.733 15.267 C 21.360 18.244, 21.500 22.739, 21.500 72 C 21.500 111.625, 21.187 126.201, 20.293 128.202 C 18.776 131.598, 14.326 133.937, 7.946 134.693 C 4.728 135.074, 3 135.754, 3 136.639 C 3 137.744, 6.955 138, 24 138 L 45 138 45 141 C 45 143.507, 45.398 144, 47.421 144 C 49.309 144, 49.989 143.325, 50.515 140.933 C 51.733 135.386, 53.792 135.400, 71.762 141.075 C 79.236 143.435, 82.821 144, 90.330 144 C 99.721 144, 108.319 142.224, 112.800 139.359 C 114.698 138.145, 120.242 137.903, 144.550 137.970 C 169.134 138.039, 174 137.821, 174 136.655 C 174 135.690, 172.146 135.049, 167.998 134.579 C 159.947 133.668, 157.305 131.296, 155.939 123.747 C 155.279 120.102, 155.008 99.584, 155.199 67.747 C 155.527 12.979, 155.530 12.950, 161.238 10.214 C 162.735 9.497, 166.218 8.643, 168.979 8.316 C 172.256 7.928, 174 7.249, 174 6.361 C 174 5.259, 170.331 5.005, 154.750 5.026 L 135.500 5.051 129.772 17.776 L 124.044 30.500 124.022 17.750 C 124.001 5.703, 123.891 5, 122.013 5 C 120.705 5, 119.549 6.145, 118.625 8.354 C 116.921 12.433, 114.516 13.198, 109.391 11.292 C 93.768 5.481, 90.674 4.818, 80.921 5.191 C 72.578 5.510, 70.501 5.971, 64.816 8.770 C 60.961 10.667, 56.413 13.989, 53.705 16.884 L 49.120 21.786 45.560 14.289 C 43.602 10.166, 42 6.389, 42 5.896 C 42 5.399, 33.328 5, 22.500 5 C 6.499 5, 3 5.250, 3 6.393 M 78.184 14.097 C 64.024 16.792, 56.613 30.884, 63.349 42.307 C 65.576 46.083, 69.651 49.688, 78.500 55.708 C 83.070 58.817, 105.251 71.083, 105.626 70.708 C 105.807 70.526, 108.697 64.347, 112.049 56.975 C 118.751 42.232, 118.832 41.471, 114.643 32.480 C 108.798 19.931, 92.007 11.465, 78.184 14.097 M 31.205 77.538 C 31.432 115.144, 31.798 127.022, 32.785 128.750 C 34.414 131.601, 37.597 133.590, 41.750 134.353 L 45 134.950 45 116.975 L 45 99 47.341 99 C 49.376 99, 49.849 99.751, 50.959 104.750 C 54.316 119.862, 59.826 127.879, 70.303 132.892 C 77.277 136.230, 81.507 137.636, 80.728 136.357 C 80.441 135.886, 69.309 111.650, 55.992 82.500 C 42.674 53.350, 31.582 29.292, 31.344 29.038 C 31.105 28.784, 31.043 50.609, 31.205 77.538 M 124.640 52.329 L 113.780 76.158 116.140 77.748 C 121.016 81.035, 127.331 88.479, 129.836 93.894 C 131.929 98.417, 132.437 101.046, 132.465 107.500 C 132.506 116.876, 129.788 123.908, 123.384 130.997 L 119.538 135.254 124.019 134.531 C 129.253 133.686, 132.421 131.890, 134.215 128.750 C 135.191 127.041, 135.574 116.409, 135.809 84.500 C 135.979 61.400, 135.979 39.350, 135.809 35.500 L 135.500 28.500 124.640 52.329 M 78.353 85.161 C 86.036 102.207, 88.350 107, 88.893 106.999 C 89.641 106.998, 96.422 92.068, 95.886 91.603 C 95.139 90.956, 87.436 86.328, 81.723 83.093 L 75.947 79.823 78.353 85.161 M 95.707 115.774 C 90.984 126.150, 86.980 135.284, 86.810 136.070 C 86.567 137.190, 87.585 137.437, 91.500 137.208 C 98.690 136.787, 102.723 135.245, 107.195 131.204 C 112.528 126.387, 114.324 122.555, 114.320 116 C 114.316 109.746, 111.859 104.092, 107.239 99.704 L 104.294 96.907 95.707 115.774" stroke="none" fill="currentColor" fill-rule="evenodd"></path></svg>');function Pe(t,e){let s=ye(e,"class",3,"");var r=He();V(()=>xe(r,0,Ce(s()))),f(t,r)}function Te(t,e){J(e,!I(e))}var Ie=(t,e)=>{t.preventDefault(),e("about")},Be=(t,e)=>{t.preventDefault(),e("about")},Oe=(t,e)=>{t.preventDefault(),e("skills")},je=(t,e)=>{t.preventDefault(),e("experience")},Ae=(t,e)=>{t.preventDefault(),e("education")},Re=(t,e)=>{t.preventDefault(),e("contact")},Ze=(t,e)=>{t.preventDefault(),e("about")},Fe=(t,e)=>{t.preventDefault(),e("skills")},Ge=(t,e)=>{t.preventDefault(),e("experience")},Ve=(t,e)=>{t.preventDefault(),e("education")},Xe=(t,e)=>{t.preventDefault(),e("contact")},Ye=k('<div class="md:hidden mt-4 space-y-2 px-8"><ul class="flex flex-col space-y-2"><li><a href="#about" class="block text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200 py-2">About</a></li> <li><a href="#skills" class="block text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200 py-2">Skills</a></li> <li><a href="#experience" class="block text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200 py-2">Experience</a></li> <li><a href="#education" class="block text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200 py-2">Education</a></li> <li><a href="#contact" class="block text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200 py-2">Contact</a></li></ul></div>'),qe=k('<nav class="fixed top-0 left-0 w-full bg-white dark:bg-gray-800 shadow-md dark:shadow-lg py-4 z-20"><div class="container mx-auto flex justify-between items-center max-w-4xl px-8"><div class="flex items-center"><a href="#top"><!></a></div> <div class="md:hidden flex items-center"><!> <button class="ml-4 text-gray-600 dark:text-gray-300 focus:outline-none"><!></button></div> <div class="hidden md:flex items-center space-x-4"><ul class="flex space-x-4"><li><a href="#about" class="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200">About</a></li> <li><a href="#skills" class="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200">Skills</a></li> <li><a href="#experience" class="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200">Experience</a></li> <li><a href="#education" class="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200">Education</a></li> <li><a href="#contact" class="text-gray-600 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-200">Contact</a></li></ul> <!></div></div> <!></nav>');function Je(t,e){X(e,!0);let s=me(!1),r;Y(()=>{if(r){const i=new ResizeObserver(()=>{Q.set(r.offsetHeight)});return i.observe(r),()=>i.disconnect()}});function a(i){const h=document.getElementById(i);if(h&&r){const p=r.offsetHeight,_=h.getBoundingClientRect().top+window.scrollY;window.scrollTo({top:_-p,behavior:"smooth"}),J(s,!1)}}var v=qe(),o=l(v),c=l(o),d=l(c);d.__click=[Ie,a];var U=l(d);Pe(U,{class:"h-8 w-auto"}),n(d),n(c);var w=u(c,2),j=l(w);F(j);var M=u(j,2);M.__click=[Te,s];var W=l(M);{var ee=i=>{Se(i,{size:28})},te=i=>{De(i,{size:28})};B(W,i=>{I(s)?i(ee):i(te,!1)})}n(M),n(w);var A=u(w,2),L=l(A),D=l(L),ae=l(D);ae.__click=[Be,a],n(D);var E=u(D,2),re=l(E);re.__click=[Oe,a],n(E);var N=u(E,2),oe=l(N);oe.__click=[je,a],n(N);var S=u(N,2),le=l(S);le.__click=[Ae,a],n(S);var R=u(S,2),ne=l(R);ne.__click=[Re,a],n(R),n(L);var se=u(L,2);F(se),n(A),n(o);var ie=u(o,2);{var ce=i=>{var h=Ye(),p=l(h),_=l(p),de=l(_);de.__click=[Ze,a],n(_);var z=u(_,2),ue=l(z);ue.__click=[Fe,a],n(z);var H=u(z,2),ve=l(H);ve.__click=[Ge,a],n(H);var P=u(H,2),fe=l(P);fe.__click=[Ve,a],n(P);var Z=u(P,2),he=l(Z);he.__click=[Xe,a],n(Z),n(p),n(h),f(i,h)};B(ie,i=>{I(s)&&i(ce)})}n(v),$e(v,i=>r=i,()=>r),f(t,v),q()}ge(["click"]);var Ke=k('<div class="relative min-h-screen"><!> <!></div>');function ot(t,e){X(e,!0);const[s,r]=K(),a=()=>O($,"$theme",s),v=()=>O(Q,"$headerHeight",s);Y(()=>{typeof document<"u"&&document.documentElement.classList.toggle("dark",a()==="dark")});var o=Ke(),c=l(o);Je(c,{});var d=u(c,2);ke(d,()=>e.children),n(o),V(()=>be(o,`padding-top: ${v()??""}px;`)),f(t,o),q(),r()}export{ot as component,rt as universal};
